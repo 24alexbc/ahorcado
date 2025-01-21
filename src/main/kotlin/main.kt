1ffd6ff207170e24a69d995c78558481a8d1fd85
alex <a24alexandrebc@iessanclemente.net>
2025-01-21 10:46:04 +0100

    Add files via upload

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,57 @@
+package main.kotlin
+
+import kotlin.random.Random
+
+fun main() {
+    val lista = listOf(
+        "limon", "fresa", "sandia", "platano", "melon",
+        "uva", "kiwi", "naranja", "piña", "pera", "cereza"
+    )
+    val listaAleatoria = lista[Random.nextInt(lista.size)]
+
+    val palabraOculta = MutableList(listaAleatoria.length) { '*' }
+
+    var intentos = 6
+    println()
+    println("Cargando juego , espera ...")
+    Thread.sleep(1000)
+    DibujoAhorcado.dibujar(1)
+    println()
+    println("Bienvenido al ahorcado ,tienes $intentos intentos para adivinar una fruta")
+    println()
+
+    while (intentos > 0) {
+        println("Tu fruta es: ${palabraOculta.joinToString("")}")
+
+        print("Introduce tu letra: ")
+        val adivinando = readln().lowercase()
+        if (adivinando.length != 1) {
+            println("Escribe solo una letra")
+            continue
+        }
+
+        val letra = adivinando[0]
+
+        if (letra in listaAleatoria) {
+            for (i in listaAleatoria.indices) {
+                if (listaAleatoria[i] == letra) {
+                    palabraOculta[i] = letra
+                }
+            }
+            println("Si! la letra $letra esta en la palabra.")
+        } else {
+            intentos--
+            println("No! cuidado solo te quedan $intentos intentos.")
+            DibujoAhorcado.dibujar(7 - intentos)
+        }
+
+        if (!palabraOculta.contains('*')) {
+            println("Felicidades la fruta era: $listaAleatoria")
+            break
+        }
+    }
+
+    if (intentos == 0) {
+        println("DEP. La fruta era: $listaAleatoria")
+    }
+}
